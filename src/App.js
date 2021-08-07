0034edd41147290539672e4f3f9b26ec9f2e9207
zeeshan191 <80030867+zeeshan191@users.noreply.github.com>
2021-08-07 22:10:22 +0530

    cooolhaihum

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
+import Pdf from './Pdf'
 import './App.css';
+import FnPdf from './FnPdf';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+    <FnPdf/>
+ </>
   );
 }
 
---
+++ src/Po.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+export class Po extends React.PureComponent {
+  render() {
+
+
+    return (
+        
+        
+    <div className="container">
+<div className="row">
+                    <div className="col-sm-12 print-div" id="printarea">
+                    <div className="row ">
+                    <div className="col-sm-12 print-div">
+                    <div class="print-quot">
+                    <div class="row ">
+                    <div class="col-sm-3 logo-div">
+                    <img src="loginpage.png" className="img-responsive" style={{width:'100%'}}/>
+                </div>
+                <div className="col-sm-4 print-quot1">
+                <h1>Purchase Order</h1>
+            </div>
+            <div className="col-sm-5">
+            <div className="print-quot3">
+
+            </div> 
+            </div>
+            </div>
+
+
+                    </div>
+                    </div> 
+                    </div>
+                    </div> 
+        </div> 
+        
+        </div>
+         
+        );
+      }
+    }
